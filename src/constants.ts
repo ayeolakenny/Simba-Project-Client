92f6dde1481aaea74331a8b7708e119c38f8592c
ayeolakenny <ayeolakenny@gmail.com>
2022-02-18 15:13:39 +0100

    alot of changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const SERVER_URI_GRAPHQL =
   'https://simba-project01.herokuapp.com/graphql'
-// 'http://localhost:4000/graphql'
 
 export const currencies = [
   { value: 'USD', label: 'USD' },
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -54,7 +54,7 @@ const Login = () => {
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign up for an account
+              Login to your account
             </h2>
           </div>
           <Formik
